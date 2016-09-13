275432b145dcd1ab5cc7dcde010f1f19d80a10e4
Caio Forchesatto <c145573@g.unicamp.br>
2016-09-13 13:21:15 -0300

    Revisando
    Cometi um erro, mas ja arrumei.

---
+++ PrevidenciaSocial.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package previdenciasocial;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PrevidenciaSocial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      System.out.println("\n Bem Vindo ao Sistema da Previdencia Social \n");
+        
+    }
+    
+}
+
